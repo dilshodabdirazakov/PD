befc5da9c2fa488639f0c52fd9a3ddc33f4dc792
Dilshod <abdirazakovdilshodbek@gmail.com>
2022-01-10 08:07:34 +0500

    added template

--- PD2/main_app/admin.py
+++ PD2/main_app/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+from .models import Load
+
+class LoadAdmin(admin.ModelAdmin):
+    list_display = ("origin", "destination", "email", "phone_number")
+
+admin.site.register(Load, LoadAdmin)
 
--- PD2/main_app/models.py
+++ PD2/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Load(models.Model):
+    origin = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone_number = models.FloatField()
